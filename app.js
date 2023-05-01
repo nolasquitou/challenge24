dfb24b3fdf6de4d4cb8ce009f31cf1612fd80aec
Nolasquito <nolascopalavecino.com>
2023-05-01 00:38:39 -0300

    Challenge 24 Terminadaso

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ let nothing = 0;
 let cofres = 0;
 
 for (let index = 0; index < 10; index++) {
-    cofres = Math.round(Math.random()* 3)
+    cofres = Math.floor(Math.random() * 3)
 
-    if(cofres == 0){
+    if (cofres == 0) {
         console.log("Encontraste + 10 de oro");
-        gold = gold + 10
-    }else if(cofres == 1){
+        console.log(cofres);
+        gold = gold + 10;
+    } else if (cofres == 1) {
         console.log("Encontraste +2 posiones");
-        potion = potion + 2
-    }else if(cofres == 3){
+        console.log(cofres);
+        potion = potion + 2;
+    } else if (cofres == 2) {
         console.log("No encontraste nada");
-        nothing = nothing + 1
+        console.log(cofres);
+        nothing = nothing + 1;
     }
 }
 console.log(`Tienes ${gold} de oro`);
